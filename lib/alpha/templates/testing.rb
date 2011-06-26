66a8a80527379ac06ccc06ae3a17bf6dadee6dbe
Matthew Solt <mattsolt@gmail.com>
2011-06-26 19:07:39 -0400

    updated haml and testing templates

--- lib/alpha/templates/testing.rb
+++ lib/alpha/templates/testing.rb
@@ -5,11 +5,3 @@ empty_directory "spec/helpers"
 empty_directory "features/step_definitions"
 empty_directory "features/support"
 
-inject_into_file 'config/application.rb', :after => "# Configure the default encoding used in templates for Ruby 1.9.\n" do
-<<-RUBY
-    config.generators do |g|
-      g.test_framework :rspec
-    end
-RUBY
-end
-
